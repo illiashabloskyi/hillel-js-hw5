b0d72b312a425c8c0ff2de4c847093e53237c7a7
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-01 00:06:02 +0300

    update script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// Переписати код нижче із використанням конструкції switch…case
+
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
+// if(numOrStr === null) {
+//     console.log('ви відмінили')
+// } else if( numOrStr.trim() === '' ) {
+//     console.log('Empty String');
+// } else if( isNaN( +numOrStr ) ) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
+
+let numOrStr = prompt("input number or string");
+console.log(numOrStr);
+
+switch (true){
+  case numOrStr === null:
+    console.log("ви відмінили");
+    break;
+  case numOrStr === "":
+    console.log("Empty String");
+    break;
+  case isNaN(+numOrStr):
+    console.log("number is Ba_NaN");
+    break;
+  default:
+    console.log("OK!");
+    break;
+}
